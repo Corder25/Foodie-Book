54f97905ceb8c0022c49fca5bc0c5dd653799f09
chelle <maphaiyarath@gmail.com>
2021-01-21 19:25:01 -0600

    adding rant model and updating user model

---
+++ models/rant.js
@@ -0,0 +1,38 @@
+// create our Rant model
+module.exports = function(sequelize, DataTypes) {
+    const Rant = sequelize.define("Rant", {
+      restaurant_name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+              len: [3]
+          }    
+      },
+      body: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+          validate: {
+              len: [1]
+          }
+      },
+      rating: {
+          type: DataTypes.INTEGER,
+          validate: {
+              min: 1,
+              max: 10
+          }
+      }
+    });
+  
+    Rant.associate = function(models) {
+        // a Rant belongs to a User
+        // a Rant can't be created without a User due to the foreign key constraint
+        Rant.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+  
+    return Rant;
+  };
